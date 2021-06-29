aa5654372074d063e8239f50a5dbb45945941496
meyospeh <meyoseph@gmail.com>
2021-06-28 19:47:03 -0500

    react function component to upload image to s3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import S3 from "react-aws-s3";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fileInput = useRef();
+    const handleClick = event => {
+        event.preventDefault();
+        let file = fileInput.current.files[0];
+        let newFileName = fileInput.current.files[0].name;
+        const config = {
+            bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
+            region: process.env.REACT_APP_S3_REGION,
+            accessKeyId: process.env.REACT_APP_S3_ACCESS_ID,
+            secretAccessKey: process.env.REACT_APP_S3_ACCESS_KEY
+        }
+        const Client = new S3(config);
+        Client.uploadFile(file, newFileName).then(data => {
+            if(data.status === 204){
+                console.log("success");
+                console.log(data.location);
+            }else{
+                console.log("fail to upload");
+            }
+        });
+    }
+    return (
+        <>
+            <form className='upload-steps' onSubmit={handleClick}>
+                <label>
+                    select your file:
+                    <input type='file' ref={fileInput} />
+                </label>
+                <br/>
+                <button type='submit'>Upload File</button>
+            </form>
+        </>
+    )
 }
 
 export default App;
